07ca9430a4406b54530b1f574c4ec15c35af7d85
NeoLH <leohdz2905@gmail.com>
2024-08-29 12:39:21 -0600

    Archivos actualizados

--- practicas/p03/config/inclusion_archivos/encabezado.inc.php
+++ practicas/p03/config/inclusion_archivos/encabezado.inc.php
@@ -12,10 +12,10 @@
     </head>
     <body>
     <?php
-        $variableext=« Este texto proviene del archivo incluido";
+        $variableext="Este texto proviene del archivo incluido";
         echo "<div><h1 style=\"border-width:5;border-style:double;background-color:#ffcc99;\">
-        Bienvenido en el sitio $variable1 </h1>"; ← (2)
+        Bienvenido en el sitio $variable1 </h1>";
         echo "<h3> $variableext</h3>";
-        echo "Nombre de archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp;&nbsp;"; ← (3)
-        echo " Nombre del archivo incluido : ", __FILE__ ,"</div> "; ← (3)
+        echo "Nombre de archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp;&nbsp;";
+        echo " Nombre del archivo incluido : ", __FILE__ ,"</div> ";
     ?>
