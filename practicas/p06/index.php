b91294f3d000bea869347fd3fc5edecd63527e0d
NeoLH <leohdz2905@gmail.com>
2024-09-18 20:48:01 -0600

    Ejercicio 2

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -6,21 +6,30 @@
     <title>Práctica 6</title>
 </head>
 <body>
+   <?php
+        include 'src/funciones.php';
+   ?> 
+    <!--Ejericio 1-->
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
-    <form action="http://localhost/tecweb/practicas/p06/index.php" method="get">
-    </form>
     <?php
-    if(isset($_GET['numero']))
-    {
-        $num = $_GET['numero'];
-        if ($num%5==0 && $num%7==0){
-            echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-        }
-        else{
-            echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-        }
-    }
+        numero();
     ?>
+    <!--Ejericio 2-->
+    <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una secuencia compuesta por:</p>
+    <h3 style="text-align: center;">impar, par, impar</h3>
+    <p style="text-align: center;">990, 382, 786</p>
+    <p style="text-align: center;">422, 361, 473</p>
+    <p style="text-align: center;">392, 671, 914</p>
+    <p style="text-align: center;">213, 744, 911</p>
+    <p>Estos números deben almacenarse en una matriz de Mx3, donde M es el número de filas y
+    3 el número de columnas. Al final muestra el número de iteraciones y la cantidad de
+    números generados:</p>
+    <p style="text-align: center;">12 números obtenidos en 4 iteraciones</p>
+    <?php
+        secuencia();
+    ?>
+    <!--Ejericio 3-->
 </body>
 </html>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -1,13 +1,45 @@
-//solucion del ejercicio 1
+<!--Solucion del Ejercicio 1-->
 <?php
-    if(isset($_GET['numero']))
-    {
-        $num = $_GET['numero'];
-        if ($num%5==0 && $num%7==0){
-            echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+    function numero(){
+        if(isset($_GET['numero']))
+        {
+            $num = $_GET['numero'];
+            if ($num%5==0 && $num%7==0){
+                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+            }
+            else{
+                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+            }
         }
-        else{
-            echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+    }
+?>
+
+<!--Solucion del Ejercicio 2-->
+<?php
+function secuencia() {
+    $matriz = [];
+    $iter = 0;
+    $secuencia = false;
+
+    while (!$secuencia) {
+        $iter++;
+        $fila = [];
+        
+        //numeros aleatorios
+        for ($i = 0; $i < 3; $i++) {
+            $fila[] = rand(1, 999);
         }
+        if ($fila[0] % 2 != 0 && $fila[1] % 2 == 0 && $fila[2] % 2 != 0) {
+            $secuencia = true;
+        }
+        $matriz[] = $fila;
+    }
+    //Mostrar
+    echo "<h2>Solucion:</h2>";
+    foreach ($matriz as $fila) {
+        echo implode(", ", $fila)."<br>";
+    }
+    $numeros_generados = $iter*3;
+    echo "<h3>$numeros_generados números obtenidos en $iter iteraciones.</h3>";
     }
 ?>
