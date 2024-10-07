71feee8843ae699324c6f699226393ccb0630972
NeoLH <leohdz2905@gmail.com>
2024-10-07 17:06:47 -0600

    Modificiaciones del ejemplo 1 y Ejemplo 2 Agregado

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -1,3 +1,4 @@
+/*
 function getDatos(){
     var nombre=prompt('Nombre: ', '');
     var edad=prompt('Edad: ', 0);
@@ -6,8 +7,16 @@ function getDatos(){
     div1.innerHTML = '<h3>Nombre: ' + nombre + '</h3>';
     var div2 = document.getElementById('edad');
     div2.innerHTML = '<h3>Edad: ' + edad + '</h3>';
-}
-function holaMundo(){
+}*/
+function ejercicio1(){
     var div3 = document.getElementById('Ejemplo1'); 
     div3.innerHTML = '<h3>Hola Mundo</h3>';
+}
+function ejercicio2(){
+    var nombre='Juan';
+    var edad=10;
+    var altura=1.92;
+    var casado=true;
+    var div4 = document.getElementById('Ejemplo2');
+    div4.innerHTML=nombre+'<br>'+edad+'<br>'+altura+'<br>'+casado+'<br>';
 }
