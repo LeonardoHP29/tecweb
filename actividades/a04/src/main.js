e015a5d9b7e4e1b31f9638160094d8826a02d58c
NeoLH <leohdz2905@gmail.com>
2024-10-07 16:38:02 -0600

    Ejemplo 1 Agregado

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -6,4 +6,8 @@ function getDatos(){
     div1.innerHTML = '<h3>Nombre: ' + nombre + '</h3>';
     var div2 = document.getElementById('edad');
     div2.innerHTML = '<h3>Edad: ' + edad + '</h3>';
-} 
+}
+function holaMundo(){
+    var div3 = document.getElementById('Ejemplo1'); 
+    div3.innerHTML = '<h3>Hola Mundo</h3>';
+}
