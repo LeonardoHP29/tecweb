3f07ed639d7ebb1f4f09f2afb046a6d79040ffa0
NeoLH <leohdz2905@gmail.com>
2024-10-08 11:19:13 -0600

    Ejemplo 18 Agregado

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -223,3 +223,21 @@ function ejercicio17(){
     var div19=document.getElementById('Ejemplo17');
     div19.innerHTML='<p>'+r+'</p>';
 }
+function ejercicio18(){
+    function convertirCastellano(x){
+        switch(x){
+            case 1:return "uno";
+            case 2:return "dos";
+            case 3:return "tres";
+            case 4:return "cuatro";
+            case 5:return "cinco";
+            default: return "valor incorrecto";
+        }
+    }
+    var valor;
+    valor=prompt('Ingresa un valor entre 1 y 5','');
+    valor=parseInt(valor);
+    var r=convertirCastellano(valor);
+    var div20=document.getElementById('Ejemplo18');
+    div20.innerHTML='<p>'+r+'</p>';
+}
