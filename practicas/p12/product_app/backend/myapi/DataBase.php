f3e3196490ffd96d1b781e21f7c64def3c51ab39
NeoLH <leohdz2905@gmail.com>
2024-11-09 20:07:23 -0600

    Practica 12 completa Agregada

---
+++ practicas/p12/product_app/backend/myapi/DataBase.php
@@ -0,0 +1,25 @@
+<?php
+namespace TECWEB\MYAPI;
+
+abstract class DataBase {
+    protected $conexion;
+    protected $data;
+
+    public function __construct($db, $user, $pass) {
+        $this->conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+        $this->data = array();
+        if(!$this->conexion) {
+            die('¡Base de datos NO conextada!');
+        }
+    }
+
+    public function getData() {
+        return json_encode($this->data, JSON_PRETTY_PRINT);
+    }
+}
+?>
